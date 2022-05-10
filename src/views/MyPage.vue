082536d37538e9ed4c1d7f5c072ec951743ee070
justh00k <mail2h00k@gmail.com>
2022-05-10 12:33:42 +0300

    asd

---
+++ src/views/MyPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="inputs" v-for="item in inputs" :key="item.name">
+    <input
+      :placeholder="item.holder"
+      :value="item.value"
+      @input="item.value = $event.target.value"
+    />
+    <button @click="deleteInput(item)">Удалить</button>
+  </div>
+  <button @click="addInput">Добавить</button>
+  asdasd
+</template>
+
+<script>
+export default {
+  name: "MyPage",
+  data() {
+    return {
+      inputs: [
+        { name: "input1", id: 0, holder: "something", value: "Щукинская" },
+        { name: "input2", id: 1, holder: "something2" },
+      ],
+      inputsNum: "",
+    };
+  },
+  methods: {
+    addInput() {
+      this.inputsNum = this.inputs.length + 1;
+      this.inputs.push({
+        name: `input${this.inputsNum}`,
+        holder: `something${this.inputsNum}`,
+        id: `${this.inputsNum - 1}`,
+        value: "",
+      });
+    },
+    deleteInput(item) {
+      this.inputs.splice(item.id, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
